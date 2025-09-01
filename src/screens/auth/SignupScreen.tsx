cdefcc959cdf543c2c8b62abaf114b5a2a035879
j2an777 <ha991104@gmail.com>
2025-09-01 18:04:01 +0900

    fix: 회원가입 페이지 적용

--- src/screens/auth/SignupScreen.tsx
+++ src/screens/auth/SignupScreen.tsx
@@ -1,11 +1,56 @@
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
+
+import CustomButton from '@/components/CustomButton';
+import InputField from '@/components/InputField';
+import validation from '@/utils/validation';
+import useForm from '@/hooks/useForm';
 
 const SignupScreen = () => {
+  const signup = useForm({
+    initialValue: {
+      email: '',
+      password: '',
+      passwordConfirm: '',
+    },
+    validate: validation,
+  });
+
   return (
-    <SafeAreaView>
-      <Text>SignupScreen</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <InputField
+          placeholder="이메일"
+          touched={signup.touched.email}
+          {...signup.getTextInputProps('email')}
+        />
+        <InputField
+          secureTextEntry
+          textContentType="oneTimeCode"
+          placeholder="비밀번호"
+          touched={signup.touched.password}
+          {...signup.getTextInputProps('password')}
+        />
+        <InputField
+          secureTextEntry
+          placeholder="비밀번호 확인"
+          touched={signup.touched.passwordConfirm}
+          {...signup.getTextInputProps('passwordConfirm')}
+        />
+      </View>
+      <CustomButton label="회원가입" variant="filled" size="large" />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 30,
+  },
+  inputContainer: {
+    gap: 20,
+    marginBottom: 30,
+  },
+});
 
 export default SignupScreen;
