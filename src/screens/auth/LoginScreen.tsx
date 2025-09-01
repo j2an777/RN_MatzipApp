3917d6cd4d66aadcab60cc12639c3fd038e53c1c
j2an777 <ha991104@gmail.com>
2025-09-01 18:03:51 +0900

    fix: 로그인 페이지 적용

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -2,13 +2,35 @@ import {SafeAreaView, StyleSheet, View} from 'react-native';
 
 import CustomButton from '@/components/CustomButton';
 import InputField from '@/components/InputField';
+import validation from '@/utils/validation';
+import useForm from '@/hooks/useForm';
 
 const LoginScreen = () => {
+  const login = useForm({
+    initialValue: {
+      email: '',
+      password: '',
+    },
+    validate: validation,
+  });
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.inputContainer}>
-        <InputField placeholder="이메일을 입력해주세요." />
-        <InputField placeholder="비밀번호를 입력해주세요." />
+        <InputField
+          placeholder="이메일"
+          touched={login.touched.email}
+          errorMessage={login.errors.email}
+          {...login.getTextInputProps('email')}
+        />
+        <InputField
+          secureTextEntry
+          textContentType="oneTimeCode"
+          placeholder="비밀번호"
+          touched={login.touched.password}
+          errorMessage={login.errors.password}
+          {...login.getTextInputProps('password')}
+        />
       </View>
       <CustomButton label="로그인" variant="filled" size="large" />
     </SafeAreaView>
