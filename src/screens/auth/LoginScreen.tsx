a7ee7df8aebcddce74d148370998cfc5221da225
j2an777 <ha991104@gmail.com>
2025-09-01 16:30:08 +0900

    feat: 로그인 페이지 퍼블리싱

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,11 +1,29 @@
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
+
+import CustomButton from '@/components/CustomButton';
+import InputField from '@/components/InputField';
 
 const LoginScreen = () => {
   return (
-    <SafeAreaView>
-      <Text>LoginScreen</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <InputField placeholder="이메일을 입력해주세요." />
+        <InputField placeholder="비밀번호를 입력해주세요." />
+      </View>
+      <CustomButton label="로그인" variant="filled" size="large" />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 30,
+  },
+  inputContainer: {
+    gap: 20,
+    marginBottom: 30,
+  },
+});
 
 export default LoginScreen;
