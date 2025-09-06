4a8c46014956ef2bc06e4bbfa0ba5fb7ae7776fa
JongoneChae <wade.bdry@hanwha.com>
2025-09-06 16:33:47 +0900

    fix: 임시 로그아웃 텍스트 추가하여 로그아웃 테스트

--- src/screens/map/MapHomeScreen.tsx
+++ src/screens/map/MapHomeScreen.tsx
@@ -1,12 +1,16 @@
-import {SafeAreaView, Text} from 'react-native';
+import { SafeAreaView, Text } from 'react-native';
 
 import DrawerButton from '@/components/DrawerButton';
+import useAuth from '@/hooks/queries/useAuth';
 
 const MapHomeScreen = () => {
+  const { logoutMutation } = useAuth();
+
   return (
     <SafeAreaView>
       <Text>MapHomeScreen</Text>
       <DrawerButton />
+      <Text onPress={() => logoutMutation.mutate(null)}>로그아웃</Text>
     </SafeAreaView>
   );
 };
