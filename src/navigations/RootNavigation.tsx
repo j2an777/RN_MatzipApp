2e48d6ccca5e90d75afad39e0985c2a1d7590eb6
JongoneChae <wade.bdry@hanwha.com>
2025-09-06 16:35:47 +0900

    fix: 최상단에서 auth를 기준으로 네비게이션하도록 수정

--- src/navigations/RootNavigation.tsx
+++ src/navigations/RootNavigation.tsx
@@ -1,8 +1,9 @@
 import DrawerNavigation from './DrawerNavigation';
 import AuthNavigation from './AuthNavigation';
+import useAuth from '@/hooks/queries/useAuth';
 
 const RootNavigation = () => {
-  const isLogin = false;
+  const { isLogin } = useAuth();
 
   return <>{isLogin ? <DrawerNavigation /> : <AuthNavigation />}</>;
 };
