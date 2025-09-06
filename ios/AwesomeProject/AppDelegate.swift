b15a528351da551176109b1ddb348b23c4f54495
JongoneChae <wade.bdry@hanwha.com>
2025-09-06 16:49:28 +0900

    feat: react-native-maps를 ios와 android에 적용하기위한 세팅

--- ios/AwesomeProject/AppDelegate.swift
+++ ios/AwesomeProject/AppDelegate.swift
@@ -2,6 +2,7 @@ import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
+import GoogleMaps
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyA3qwsO_QcnAkU14ExCTw_Jd99gsZ2Q5o0");
     let delegate = ReactNativeDelegate()
     let factory = RCTReactNativeFactory(delegate: delegate)
     delegate.dependencyProvider = RCTAppDependencyProvider()
