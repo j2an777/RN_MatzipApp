a86972f3719fa19aab815600674c4b9a7c989c5e
j2an777 <ha991104@gmail.com>
2025-11-03 20:08:17 +0900

    fix: 생성한 이미지 추가 버튼 및 점수와 마커 선택 input 추가 적용

--- src/screens/map/AddLocationScreen.tsx
+++ src/screens/map/AddLocationScreen.tsx
@@ -1,68 +1,101 @@
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { StackScreenProps } from '@react-navigation/stack';
 import { ScrollView, StyleSheet } from 'react-native';
-
-import { MapStackParamList } from '@/types/navigation';
-import CustomButton from '@/components/CustomButton';
-import { validateAddPost } from '@/utils/validation';
-import useGetAddress from '@/hooks/useGetAddress';
-import InputField from '@/components/InputField';
-import useForm from '@/hooks/useForm';
 import DatePicker from 'react-native-date-picker';
 import { useState } from 'react';
+
+import MarkerColorInput from '@/components/MarkerColorInput';
+import FixedBottomCTA from '@/components/FixedBottomCTA';
+import { MapStackParamList } from '@/types/navigation';
 import { getDateWithSeparator } from '@/utils/getDate';
+import CustomButton from '@/components/CustomButton';
+import { validateAddPost } from '@/utils/validation';
+import useImagePicker from '@/hooks/useImagePicker';
+import useGetAddress from '@/hooks/useGetAddress';
+import usePermission from '@/hooks/usePermission';
+import InputField from '@/components/InputField';
+import ScoreInput from '@/components/ScoreInput';
+import ImageInput from '@/components/ImageInput';
+import { colors } from '@/constants/colors';
+import useForm from '@/hooks/useForm';
 
 type Props = StackScreenProps<MapStackParamList, 'AddLocation'>;
 
 const AddLocationScreen = ({ route }: Props) => {
   const { location } = route.params;
+  const inset = useSafeAreaInsets();
   const address = useGetAddress(location);
   const [openDate, setOpenDate] = useState(false);
+  const { handleChangeOpenPicker } = useImagePicker();
+
+  usePermission('PHOTO');
 
   const postForm = useForm({
     initialValue: {
       title: '',
       description: '',
       date: new Date(),
+      color: colors.PINK_400,
+      score: 3,
     },
     validate: validateAddPost,
   });
 
+  const handleSubmit = () => {};
+
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <InputField value={address} disabled />
-      <CustomButton
-        label={getDateWithSeparator(postForm.values.date, '. ')}
-        variant="outlined"
-        onPress={() => setOpenDate(true)}
-      />
-      <InputField
-        placeholder="제목을 입력하세요."
-        errorMessage={postForm.errors.title}
-        touched={postForm.touched.title}
-        {...postForm.getTextInputProps('title')}
-      />
-      <InputField
-        multiline
-        placeholder="기록하고 싶은 내용을 입력하세요. (선택)"
-        errorMessage={postForm.errors.description}
-        touched={postForm.touched.description}
-        {...postForm.getTextInputProps('description')}
-      />
-      <DatePicker
-        modal
-        locale="ko"
-        mode="date"
-        date={postForm.values.date}
-        open={openDate}
-        onCancel={() => setOpenDate(false)}
-        title={null}
-        cancelText="취소"
-        confirmText="완료"
-        onConfirm={date => {
-          postForm.onChange('date', date);
-        }}
-      />
-    </ScrollView>
+    <>
+      <ScrollView
+        contentContainerStyle={[
+          styles.container,
+          { paddingBottom: inset.bottom + 100 },
+        ]}>
+        <InputField value={address} disabled />
+        <CustomButton
+          label={getDateWithSeparator(postForm.values.date, '. ')}
+          variant="outlined"
+          onPress={() => setOpenDate(true)}
+        />
+        <InputField
+          placeholder="제목을 입력하세요."
+          errorMessage={postForm.errors.title}
+          touched={postForm.touched.title}
+          {...postForm.getTextInputProps('title')}
+        />
+        <InputField
+          multiline
+          placeholder="기록하고 싶은 내용을 입력하세요. (선택)"
+          errorMessage={postForm.errors.description}
+          touched={postForm.touched.description}
+          {...postForm.getTextInputProps('description')}
+        />
+        <MarkerColorInput
+          color={postForm.values.color}
+          score={postForm.values.score}
+          onChangeColor={value => postForm.onChange('color', value)}
+        />
+        <ScoreInput
+          score={postForm.values.score}
+          onChangeScore={value => postForm.onChange('score', value)}
+        />
+        <DatePicker
+          modal
+          locale="ko"
+          mode="date"
+          date={postForm.values.date}
+          open={openDate}
+          onCancel={() => setOpenDate(false)}
+          title={null}
+          cancelText="취소"
+          confirmText="완료"
+          onConfirm={date => {
+            postForm.onChange('date', date);
+          }}
+        />
+        <ImageInput onChange={handleChangeOpenPicker} />
+      </ScrollView>
+      <FixedBottomCTA label="저장" onPress={handleSubmit} />
+    </>
   );
 };
 
