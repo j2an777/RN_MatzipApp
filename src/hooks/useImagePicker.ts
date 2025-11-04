565a53b7bc0f0f4ac1a040c449a42ecfc93b2fd8
j2an777 <ha991104@gmail.com>
2025-11-04 12:57:35 +0900

    fix: 권한 허용에 대한 에러 처리 추가 및 이미지 삭제 콜백함수 추가

--- src/hooks/useImagePicker.ts
+++ src/hooks/useImagePicker.ts
@@ -1,4 +1,5 @@
 import ImageCropPicker from 'react-native-image-crop-picker';
+import Toast from 'react-native-toast-message';
 import { useState } from 'react';
 
 import getFormDataImages from '@/utils/getFormDataImages';
@@ -14,21 +15,36 @@ const useImagePicker = () => {
     setImageUris(prev => [...prev, ...uris.map(uri => ({ uri }))]);
   };
 
+  const deleteImageUri = (uri: string) => {
+    const newImageUris = imageUris.filter(image => image.uri !== uri);
+    setImageUris(newImageUris);
+  };
+
   const handleChangeOpenPicker = () => {
     ImageCropPicker.openPicker({
       mediaType: 'photo',
       multiple: true,
       includeBase64: true,
       maxFiles: 5,
-    }).then(images => {
-      const formData = getFormDataImages('images', images);
-      mutate(formData, {
-        onSuccess: data => addImageUris(data),
+    })
+      .then(images => {
+        const formData = getFormDataImages('images', images);
+        mutate(formData, {
+          onSuccess: data => addImageUris(data),
+        });
+      })
+      .catch(e => {
+        if (e.code !== 'E_PICKER_CANCELLED') {
+          Toast.show({
+            type: 'error',
+            text1: '권한을 허용했는지 확인해주세요.',
+            position: 'bottom',
+          });
+        }
       });
-    });
   };
 
-  return { handleChangeOpenPicker, imageUris };
+  return { handleChangeOpenPicker, imageUris, deleteImageUri };
 };
 
 export default useImagePicker;
