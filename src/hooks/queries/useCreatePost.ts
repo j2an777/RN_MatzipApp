9172ac79718b2fb10c3a07abd3e130d42bac96be
j2an777 <ha991104@gmail.com>
2025-11-04 14:24:02 +0900

    fix: marker 등록 성공시 invalidQueries로 refresh 데이터 가져오도록 수정

--- src/hooks/queries/useCreatePost.ts
+++ src/hooks/queries/useCreatePost.ts
@@ -1,11 +1,18 @@
 import { useMutation } from '@tanstack/react-query';
 
 import { UseMutationCustomOptions } from '@/types/api';
+import { queryKeys } from '@/constants/keys';
+import queryClient from '@/api/queryClient';
 import { createPost } from '@/api/post';
 
 const useCreatePost = (mutationOptions?: UseMutationCustomOptions) => {
   return useMutation({
     mutationFn: createPost,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [queryKeys.MARKER, queryKeys.GET_MARKERS],
+      });
+    },
     ...mutationOptions,
   });
 };
