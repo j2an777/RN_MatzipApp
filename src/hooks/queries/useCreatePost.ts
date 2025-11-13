90326cfb5b748133ac28c451c4f6a2b1bcaea6d7
j2an777 <ha991104@gmail.com>
2025-11-13 13:29:55 +0900

    fix: 해당 invalidateQueries를 setQueryData로 했을 때의 로직 추가

--- src/hooks/queries/useCreatePost.ts
+++ src/hooks/queries/useCreatePost.ts
@@ -4,14 +4,29 @@ import { UseMutationCustomOptions } from '@/types/api';
 import { queryKeys } from '@/constants/keys';
 import queryClient from '@/api/queryClient';
 import { createPost } from '@/api/post';
+// import { Marker } from '@/types/domain';
 
 const useCreatePost = (mutationOptions?: UseMutationCustomOptions) => {
   return useMutation({
     mutationFn: createPost,
-    onSuccess: () => {
+    onSuccess: newPost => {
       queryClient.invalidateQueries({
         queryKey: [queryKeys.MARKER, queryKeys.GET_MARKERS],
       });
+      // queryClient.setQueryData<Marker[]>(
+      //   [queryKeys.MARKER, queryKeys.GET_MARKERS],
+      //   oldPosts => {
+      //     const newData = {
+      //       id: newPost.id,
+      //       longitude: newPost.longitude,
+      //       latitude: newPost.latitude,
+      //       color: newPost.color,
+      //       score: newPost.score,
+      //     };
+
+      //     return oldPosts ? [...oldPosts, newData] : [newData];
+      //   },
+      // );
     },
     ...mutationOptions,
   });
