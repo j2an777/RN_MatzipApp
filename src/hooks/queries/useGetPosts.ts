c946afbfdef52de269ee7d7da372e07d106e68fd
j2an777 <ha991104@gmail.com>
2025-11-14 11:15:21 +0900

    fix: 해당 게시글에 대한 데이터 response에 대한 Promise타입 부여

--- src/hooks/queries/useGetPosts.ts
+++ src/hooks/queries/useGetPosts.ts
@@ -2,9 +2,10 @@ import { useQuery } from '@tanstack/react-query';
 
 import { UseQueryCustomOptions } from '@/types/api';
 import { queryKeys } from '@/constants/keys';
+import { Post } from '@/types/domain';
 import { getPost } from '@/api/post';
 
-const useGetPost = (id: number, queryOptions?: UseQueryCustomOptions) => {
+const useGetPost = (id: number, queryOptions?: UseQueryCustomOptions<Post>) => {
   return useQuery({
     queryFn: () => getPost(Number(id)),
     queryKey: [queryKeys.POST, queryKeys.GET_POST, id],
