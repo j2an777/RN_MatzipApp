171ad6675cfa8f370b419e9b29141160d18d5319
j2an777 <ha991104@gmail.com>
2025-12-17 18:23:31 +0900

    fix: 지도 이동 훅에 대하여 전역 상태에 location data가 있으면 해당 location으로 이동하도록 사이드 이펙트 추가

--- src/hooks/useMoveMapView.ts
+++ src/hooks/useMoveMapView.ts
@@ -1,12 +1,15 @@
 import MapView, { LatLng, Region } from 'react-native-maps';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { numbers } from '@/constants/numbers';
+import useLocationStore from '@/store/location';
 
 type Delta = Pick<Region, 'latitudeDelta' | 'longitudeDelta'>;
 
 const useMoveMapView = () => {
   const [regionDelta, setRegionDetla] = useState<Delta>(numbers.INITIAL_DELTA);
+
+  const { moveLocation } = useLocationStore();
   const mapRef = useRef<MapView | null>(null);
 
   const moveMapView = (coordinate: LatLng, delta?: Delta) => {
@@ -20,6 +23,10 @@ const useMoveMapView = () => {
     const { latitudeDelta, longitudeDelta } = region;
     setRegionDetla({ latitudeDelta, longitudeDelta });
   };
+
+  useEffect(() => {
+    if (moveLocation) moveMapView(moveLocation);
+  }, [moveLocation]);
 
   return { moveMapView, handleChangeDelta, mapRef };
 };
