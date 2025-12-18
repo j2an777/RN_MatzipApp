5c1bdf654ea23965b8aec7a0b0bb420b43b7eb75
j2an777 <ha991104@gmail.com>
2025-12-18 14:18:22 +0900

    feat: 삭제하기에 대한 alert창으로 안내 후 mutation 하도록 기능 적용

--- src/screens/feed/FeedDetailActionSheet.tsx
+++ src/screens/feed/FeedDetailActionSheet.tsx
@@ -1,4 +1,8 @@
+import { useNavigation } from '@react-navigation/native';
+
 import ActionSheet from '@/components/common/ActionSheet';
+import useDeletePost from '@/hooks/queries/useDeletePost';
+import { Alert } from 'react-native';
 
 interface FeedDetailActionSheetProps {
   id: number;
@@ -7,14 +11,40 @@ interface FeedDetailActionSheetProps {
 }
 
 const FeedDetailActionSheet = ({
+  id,
   isVisible,
   hideAction,
 }: FeedDetailActionSheetProps) => {
+  const navigation = useNavigation();
+  const { mutate } = useDeletePost();
+
+  const handleDeletePost = () => {
+    Alert.alert('삭제하시겠습니까?', '피드와 지도에서 모두 삭제됩니다.', [
+      {
+        text: '삭제',
+        onPress: () =>
+          mutate(id, {
+            onSuccess: () => {
+              hideAction();
+              navigation.goBack();
+            },
+          }),
+        style: 'destructive',
+      },
+      {
+        text: '취소',
+        style: 'cancel',
+      },
+    ]);
+  };
+
   return (
     <ActionSheet isVisible={isVisible} hideAction={hideAction}>
       <ActionSheet.Background>
         <ActionSheet.Container>
-          <ActionSheet.Button isDanger>삭제하기</ActionSheet.Button>
+          <ActionSheet.Button isDanger onPress={handleDeletePost}>
+            삭제하기
+          </ActionSheet.Button>
           <ActionSheet.Divider />
           <ActionSheet.Button>삭제하기</ActionSheet.Button>
         </ActionSheet.Container>
