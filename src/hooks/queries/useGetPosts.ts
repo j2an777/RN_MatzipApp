343fff7dc5737204d4296d6826b658290e1b88ce
j2an777 <ha991104@gmail.com>
2025-12-18 17:53:35 +0900

    fix: 게시글 가져오는 query훅에 대해 id props 옵셔널로 하는 대신 enabled 속성 추가

--- src/hooks/queries/useGetPosts.ts
+++ src/hooks/queries/useGetPosts.ts
@@ -5,10 +5,14 @@ import { queryKeys } from '@/constants/keys';
 import { Post } from '@/types/domain';
 import { getPost } from '@/api/post';
 
-const useGetPost = (id: number, queryOptions?: UseQueryCustomOptions<Post>) => {
+const useGetPost = (
+  id?: number,
+  queryOptions?: UseQueryCustomOptions<Post>,
+) => {
   return useQuery({
     queryFn: () => getPost(Number(id)),
     queryKey: [queryKeys.POST, queryKeys.GET_POST, id],
+    enabled: !!id,
     ...queryOptions,
   });
 };
