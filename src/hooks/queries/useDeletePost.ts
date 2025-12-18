e3d95792a06a301fba53a7e311c28812ceeaf25d
j2an777 <ha991104@gmail.com>
2025-12-18 14:17:55 +0900

    fix: invalidateQueries를 setQueryData로 변경해서 수정

--- src/hooks/queries/useDeletePost.ts
+++ src/hooks/queries/useDeletePost.ts
@@ -1,20 +1,25 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { UseMutationCustomOptions } from '@/types/api';
 import { queryKeys } from '@/constants/keys';
-import queryClient from '@/api/queryClient';
 import { deletePost } from '@/api/post';
+import { Marker } from '@/types/domain';
 
 const useDeletePost = (mutationOptions?: UseMutationCustomOptions) => {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: deletePost,
-    onSuccess: () => {
+    onSuccess: deleteId => {
       queryClient.invalidateQueries({
         queryKey: [queryKeys.POST, queryKeys.GET_POSTS],
       });
-      queryClient.invalidateQueries({
-        queryKey: [queryKeys.MARKER, queryKeys.GET_MARKERS],
-      });
+      // queryClient.invalidateQueries({
+      //   queryKey: [queryKeys.MARKER, queryKeys.GET_MARKERS],
+      // });
+      queryClient.setQueryData<Marker[]>(
+        [queryKeys.MARKER, queryKeys.GET_MARKERS],
+        prev => prev?.filter(marker => marker.id !== deleteId),
+      );
     },
     ...mutationOptions,
   });
