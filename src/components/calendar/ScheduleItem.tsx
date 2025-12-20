0a77d288f2bbc171dfa8774628ce0ffbf3657fcf
j2an777 <ha991104@gmail.com>
2025-12-20 16:47:46 +0900

    feat: 각 일정에 대한 저장한 피드 데이터 리스트 아이템 컴포넌트 생성

---
+++ src/components/calendar/ScheduleItem.tsx
@@ -0,0 +1,52 @@
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+
+import { colors } from '@/constants/colors';
+
+interface ScheduleItemProps {
+  subTitle: string;
+  title: string;
+}
+
+const ScheduleItem = ({ subTitle, title }: ScheduleItemProps) => {
+  return (
+    <Pressable style={styles.container}>
+      <View style={styles.line} />
+      <View style={styles.infoContainer}>
+        <Text
+          numberOfLines={1}
+          ellipsizeMode="tail"
+          style={styles.subTitleText}>
+          {subTitle}
+        </Text>
+        <Text style={styles.titleText}>{title}</Text>
+      </View>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+  },
+  line: {
+    backgroundColor: colors.PINK_700,
+    width: 6,
+    height: 50,
+    marginRight: 8,
+    borderRadius: 20,
+  },
+  infoContainer: {
+    justifyContent: 'space-evenly',
+  },
+  subTitleText: {
+    color: colors.GRAY_500,
+    fontSize: 13,
+  },
+  titleText: {
+    color: colors.BLACK,
+    fontSize: 16,
+    fontWeight: '600',
+  },
+});
+
+export default ScheduleItem;
