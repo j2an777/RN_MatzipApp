33707a558c8efa66c6a22e54b756caca1a561ecb
j2an777 <ha991104@gmail.com>
2025-12-23 17:18:30 +0900

    feat: searchLocation 페이지에서는 만든 검색창에 keyword에 대한 지역 상태 넣은 후 location과 같이 search하는 훅에 파라미터로 보내기 적용

--- src/screens/map/SearchLocationScreen.tsx
+++ src/screens/map/SearchLocationScreen.tsx
@@ -1,11 +1,37 @@
-import {SafeAreaView, Text} from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { useState } from 'react';
+
+import useSearchLocation from '@/hooks/useSearchLocation';
+import SearchInput from '@/components/map/SearchInput';
+import useUserLocation from '@/hooks/useUserLocation';
 
 const SearchLocationScreen = () => {
+  const [keyword, setKeyword] = useState('');
+  const [searchKeyword, setSearchKeyword] = useState('');
+
+  const { userLocation } = useUserLocation();
+  const res = useSearchLocation(searchKeyword, userLocation);
+
+  const handleSubmitKeyword = () => setSearchKeyword(keyword);
+
   return (
-    <SafeAreaView>
-      <Text>SearchLocationScreen</Text>
-    </SafeAreaView>
+    <View style={styles.constainer}>
+      <SearchInput
+        placeholder="검색할 장소를 입력해주세요."
+        value={keyword}
+        onChangeText={setKeyword}
+        onSubmit={handleSubmitKeyword}
+      />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  constainer: {
+    flex: 1,
+    padding: 20,
+    gap: 15,
+  },
+});
 
 export default SearchLocationScreen;
