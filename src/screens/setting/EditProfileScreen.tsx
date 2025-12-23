9fd5a4e1705ad0bb401043272ac43796c17aa7d2
j2an777 <ha991104@gmail.com>
2025-12-23 19:52:49 +0900

    feat: 프로필 수정과 세팅 홈 스크린 컴포넌트 생성

---
+++ src/screens/setting/EditProfileScreen.tsx
@@ -0,0 +1,9 @@
+import { StyleSheet, View } from 'react-native';
+
+const EditProfileScreen = () => {
+  return <View></View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default EditProfileScreen;
---
+++ src/screens/setting/SettingHomeScreen.tsx
@@ -0,0 +1,9 @@
+import { StyleSheet, View } from 'react-native';
+
+const SettingHomeScreen = () => {
+  return <View></View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default SettingHomeScreen;
