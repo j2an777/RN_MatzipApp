854d965dc0a57cca0df7f6dfb067d5db6e1638e5
j2an777 <ha991104@gmail.com>
2025-12-23 19:22:55 +0900

    chore: import 순서 적용

--- src/navigations/RootNavigation.tsx
+++ src/navigations/RootNavigation.tsx
@@ -1,6 +1,7 @@
+import useAuth from '@/hooks/queries/useAuth';
+
 import DrawerNavigation from './DrawerNavigation';
 import AuthNavigation from './AuthNavigation';
-import useAuth from '@/hooks/queries/useAuth';
 
 const RootNavigation = () => {
   const { isLogin } = useAuth();
