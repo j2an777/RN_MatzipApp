6d4876ef8d28a0bb374a47096e465196906d9679
j2an777 <ha991104@gmail.com>
2025-12-23 18:31:51 +0900

    feat: SearchRegionResult scroll view로 리스트 컴포넌트 생성

---
+++ src/components/map/SearchRegionResult.tsx
@@ -0,0 +1,110 @@
+import {
+  Dimensions,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import Ionicons from '@react-native-vector-icons/ionicons';
+
+import { RegionInfo } from '@/types/region';
+import { colors } from '@/constants/colors';
+
+const SearchRegionResult = ({ regionInfo }: { regionInfo: RegionInfo[] }) => {
+  return (
+    <View style={styles.container}>
+      <ScrollView style={styles.scrollContainer}>
+        {regionInfo.map((info, idx) => (
+          <Pressable
+            key={info.id}
+            style={[
+              styles.itemBorder,
+              idx === regionInfo.length - 1 && styles.noItemBorder,
+            ]}>
+            <View style={styles.placeNameContainer}>
+              <Ionicons name="location" size={10} color={colors.PINK_700} />
+              <Text
+                style={styles.placeText}
+                ellipsizeMode="tail"
+                numberOfLines={1}>
+                {info.place_name}
+              </Text>
+            </View>
+            <View style={styles.categoryContainer}>
+              <Text style={styles.distanceText}>
+                {(Number(info.distance) / 1000).toFixed(2)}km
+              </Text>
+              <Text style={styles.subInfoText}>{info.category_name}</Text>
+            </View>
+            <Text style={styles.subInfoText}>{info.road_address_name}</Text>
+          </Pressable>
+        ))}
+
+        {regionInfo.length === 0 && (
+          <View style={styles.emptyContainer}>
+            <Text style={styles.emptyText}>검색 결과가 없습니다.</Text>
+          </View>
+        )}
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 1,
+    borderColor: colors.GRAY_200,
+    borderRadius: 5,
+    height: Dimensions.get('screen').height / 2,
+    marginVertical: 5,
+    width: '100%',
+  },
+  scrollContainer: {
+    padding: 10,
+  },
+  itemBorder: {
+    marginHorizontal: 5,
+    paddingVertical: 10,
+    borderBottomColor: colors.GRAY_300,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    gap: 3,
+  },
+  noItemBorder: {
+    borderBottomWidth: 0,
+    paddingBottom: 20,
+  },
+  placeNameContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 5,
+  },
+  placeText: {
+    color: colors.BLACK,
+    flexShrink: 1,
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  categoryContainer: {
+    flexDirection: 'row',
+    gap: 10,
+  },
+  distanceText: {
+    color: colors.BLACK,
+  },
+  subInfoText: {
+    color: colors.GRAY_500,
+    flexShrink: 1,
+  },
+  emptyContainer: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 50,
+  },
+  emptyText: {
+    color: colors.GRAY_500,
+    fontSize: 16,
+  },
+});
+
+export default SearchRegionResult;
