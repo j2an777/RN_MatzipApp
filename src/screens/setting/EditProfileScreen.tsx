cb08a8c7f858f6918dc282cc15fcbca6d9ffba98
j2an777 <ha991104@gmail.com>
2025-12-24 15:11:41 +0900

    feat: 프로필 수정 페이지에서는 초기 useAuth를 통해 유저의 기본 설정한 이미지로 나오도록 설정 및 닉네임 수정을 위한 useForm 설정

--- src/screens/setting/EditProfileScreen.tsx
+++ src/screens/setting/EditProfileScreen.tsx
@@ -1,9 +1,104 @@
-import { StyleSheet, View } from 'react-native';
+import { Image, Platform, Pressable, StyleSheet, View } from 'react-native';
+
+import EditProfileActionSheet from '@/components/setting/EditProfileActionSheet';
+import FixedBottomCTA from '@/components/common/FixedBottomCTA';
+import Ionicons from '@react-native-vector-icons/ionicons';
+import { validateEditProfile } from '@/utils/validation';
+import InputField from '@/components/common/InputField';
+import useImagePicker from '@/hooks/useImagePicker';
+import useAuth from '@/hooks/queries/useAuth';
+import { colors } from '@/constants/colors';
+import useModal from '@/hooks/useModal';
+import useForm from '@/hooks/useForm';
+import { baseUrls } from '@/api';
 
 const EditProfileScreen = () => {
-  return <View></View>;
+  const { auth } = useAuth();
+  const imageAction = useModal();
+
+  const imagePicker = useImagePicker({
+    initialImages: auth.imageUri ? [{ uri: auth.imageUri }] : [],
+    mode: 'single',
+    onSettled: imageAction.hide,
+  });
+
+  const editProfileForm = useForm({
+    initialValue: { nickname: auth.nickname ?? '' },
+    validate: validateEditProfile,
+  });
+
+  const handlePressImage = () => {
+    imageAction.show();
+  };
+
+  return (
+    <>
+      <View style={styles.container}>
+        <View style={styles.profileContainer}>
+          <Pressable
+            style={[styles.imageContainer, styles.emptyImageContainer]}
+            onPress={handlePressImage}>
+            {imagePicker.imageUris.length === 0 ? (
+              <Ionicons
+                name="camera-outline"
+                size={30}
+                color={colors.GRAY_500}
+              />
+            ) : (
+              <Image
+                source={{
+                  uri: `${
+                    Platform.OS === 'ios' ? baseUrls.ios : baseUrls.android
+                  }/${imagePicker.imageUris[0].uri}`,
+                }}
+                style={styles.image}
+                resizeMode="cover"
+              />
+            )}
+          </Pressable>
+        </View>
+        <InputField
+          {...editProfileForm.getTextInputProps('nickname')}
+          touched={editProfileForm.touched.nickname}
+          placeholder="닉네임을 입력해주세요."
+        />
+      </View>
+      <FixedBottomCTA label="저장" onPress={() => {}} />
+      <EditProfileActionSheet
+        isVisible={imageAction.isVisible}
+        hideAction={imageAction.hide}
+        onChangeImage={imagePicker.handleChangeOpenPicker}
+      />
+    </>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  profileContainer: {
+    alignItems: 'center',
+    marginTop: 20,
+    marginBottom: 40,
+  },
+  imageContainer: {
+    width: 100,
+    height: 100,
+  },
+  emptyImageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: colors.GRAY_200,
+    borderRadius: 50,
+    borderWidth: 1,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 50,
+  },
+});
 
 export default EditProfileScreen;
