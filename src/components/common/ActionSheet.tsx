b0e24864f4f7c7407cf13bb97c19d46b24b7899a
j2an777 <ha991104@gmail.com>
2025-12-24 22:46:11 +0900

    feat: ActionSheet에서 필터와 체크박스 컴포넌트 추가

--- src/components/common/ActionSheet.tsx
+++ src/components/common/ActionSheet.tsx
@@ -79,7 +79,8 @@ const Button = ({
   return (
     <Pressable
       style={({ pressed }) => [
-        pressed ? styles.actionButtonPressed : styles.actionButton,
+        pressed && styles.actionButtonPressed,
+        styles.actionButton,
       ]}
       {...props}>
       <Text style={[styles.actionText, isDanger && styles.dangerText]}>
@@ -102,12 +103,64 @@ const Title = ({ children }: PropsWithChildren) => {
 
 const Divider = () => <View style={styles.border} />;
 
+interface FilterProps extends PressableProps {
+  children: React.ReactNode;
+  isSelected?: boolean;
+}
+
+const Filter = ({ children, isSelected, ...props }: FilterProps) => {
+  return (
+    <Pressable style={styles.filterContainer} {...props}>
+      <Text style={isSelected ? styles.filterSelectedText : styles.filterText}>
+        {children}
+      </Text>
+      <Ionicons
+        name="chevron-down"
+        size={22}
+        color={isSelected ? colors.BLUE_500 : colors.GRAY_300}
+      />
+    </Pressable>
+  );
+};
+
+interface CheckBoxProps extends PressableProps {
+  children?: React.ReactNode;
+  icon?: React.ReactNode;
+  isChecked?: boolean;
+}
+
+const CheckBox = ({
+  children,
+  icon = null,
+  isChecked = false,
+  ...props
+}: CheckBoxProps) => {
+  return (
+    <Pressable
+      {...props}
+      style={({ pressed }) => [
+        styles.checkBoxContainer,
+        pressed && styles.actionButtonPressed,
+      ]}>
+      <Ionicons
+        name={isChecked ? 'checkmark-circle' : 'checkmark-circle-outline'}
+        size={22}
+        color={colors.BLUE_500}
+      />
+      {icon}
+      <Text style={styles.checkBoxText}>{children}</Text>
+    </Pressable>
+  );
+};
+
 const ActionSheet = Object.assign(ActionMain, {
   Container,
   Button,
   Title,
   Divider,
   Background,
+  Filter,
+  CheckBox,
 });
 
 const styles = StyleSheet.create({
@@ -153,6 +206,33 @@ const styles = StyleSheet.create({
   border: {
     borderBottomColor: colors.GRAY_200,
     borderBottomWidth: 1,
+  },
+  filterContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+    gap: 5,
+  },
+  filterText: {
+    color: colors.GRAY_300,
+    fontSize: 15,
+    fontWeight: '500',
+  },
+  filterSelectedText: {
+    color: colors.BLUE_500,
+    fontSize: 15,
+    fontWeight: '500',
+  },
+  checkBoxContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+    gap: 10,
+  },
+  checkBoxText: {
+    color: colors.BLACK,
+    fontSize: 15,
   },
 });
 
