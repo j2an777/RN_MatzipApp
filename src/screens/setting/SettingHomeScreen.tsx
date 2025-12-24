f97ec0bc5f2c40497c88eb58a7ce7b751a2b15f3
j2an777 <ha991104@gmail.com>
2025-12-24 15:08:40 +0900

    feat: 설정 아이템 컴포넌트를 적용한 설정 홈 페이지 구현

--- src/screens/setting/SettingHomeScreen.tsx
+++ src/screens/setting/SettingHomeScreen.tsx
@@ -1,9 +1,45 @@
-import { StyleSheet, View } from 'react-native';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { ScrollView, StyleSheet, View } from 'react-native';
+
+import SettingItem from '@/components/setting/SettingItem';
+import { SettingStackParamList } from '@/types/navigation';
+import useAuth from '@/hooks/queries/useAuth';
+import { colors } from '@/constants/colors';
+
+type Navigation = NavigationProp<SettingStackParamList>;
 
 const SettingHomeScreen = () => {
-  return <View></View>;
+  const navigation = useNavigation<Navigation>();
+  const { logoutMutation } = useAuth();
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={styles.space} />
+        <SettingItem
+          title="프로필 수정"
+          onPress={() => navigation.navigate('EditProfile')}
+        />
+        <SettingItem title="다크 모드" />
+        <View style={styles.space} />
+        <SettingItem
+          title="로그아웃"
+          color={colors.RED_500}
+          onPress={() => logoutMutation.mutate(null)}
+        />
+      </ScrollView>
+    </SafeAreaView>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  space: {
+    height: 30,
+  },
+});
 
 export default SettingHomeScreen;
