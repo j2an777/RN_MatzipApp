a811c547da109e285d532b03a1f11a5193754917
j2an777 <ha991104@gmail.com>
2025-12-28 18:37:57 +0900

    feat: 설정 스크린에서 다크모드 클릭시 해당 액션모달창 나오도록 적용

--- src/screens/setting/SettingHomeScreen.tsx
+++ src/screens/setting/SettingHomeScreen.tsx
@@ -2,16 +2,19 @@ import { NavigationProp, useNavigation } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { ScrollView, StyleSheet, View } from 'react-native';
 
+import DarkModeActionSheet from '@/components/common/DarkModeActionSheet';
 import SettingItem from '@/components/setting/SettingItem';
 import { SettingStackParamList } from '@/types/navigation';
 import useAuth from '@/hooks/queries/useAuth';
 import { colors } from '@/constants/colors';
+import useModal from '@/hooks/useModal';
 
 type Navigation = NavigationProp<SettingStackParamList>;
 
 const SettingHomeScreen = () => {
   const navigation = useNavigation<Navigation>();
   const { logoutMutation } = useAuth();
+  const darkModeAction = useModal();
 
   return (
     <SafeAreaView style={styles.container}>
@@ -21,12 +24,16 @@ const SettingHomeScreen = () => {
           title="프로필 수정"
           onPress={() => navigation.navigate('EditProfile')}
         />
-        <SettingItem title="다크 모드" />
+        <SettingItem title="다크 모드" onPress={() => darkModeAction.show()} />
         <View style={styles.space} />
         <SettingItem
           title="로그아웃"
           color={colors.RED_500}
           onPress={() => logoutMutation.mutate(null)}
+        />
+        <DarkModeActionSheet
+          isVisible={darkModeAction.isVisible}
+          hideAction={darkModeAction.hide}
         />
       </ScrollView>
     </SafeAreaView>
