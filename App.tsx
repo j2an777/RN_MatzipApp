89608601835cda9478397441e5a11cd9f2b51991
j2an777 <ha991104@gmail.com>
2025-12-28 18:38:23 +0900

    feat: 최상위 앱의 status bar 전역 theme 값에 따라 바뀌도록 추가적용

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
-import { QueryClientProvider } from '@tanstack/react-query';
 import Toast, { BaseToast, BaseToastProps } from 'react-native-toast-message';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { StatusBar } from 'react-native';
 
 import RootNavigation from '@/navigations/RootNavigation';
+import useThemeStorage from '@/hooks/useThemeStorage';
 import queryClient from '@/api/queryClient';
 import { colors } from '@/constants/colors';
 
@@ -26,8 +28,13 @@ const toastConfig = {
 };
 
 function App() {
+  const { theme } = useThemeStorage();
+
   return (
     <QueryClientProvider client={queryClient}>
+      <StatusBar
+        barStyle={theme === 'light' ? 'dark-content' : 'light-content'}
+      />
       <RootNavigation />
       <Toast config={toastConfig} />
     </QueryClientProvider>
